31722914671efdfb1fea36e720a6188c173782ef
adiyatzzz <orderwar0@gmail.com>
2020-06-21 16:31:30 +0800

    membuat click counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
-    
+    var count = 0;
     $(".foto img").click(function () { 
         $(".foto img").css('animation', 'beundur .1s forwards');
+        
 
         setTimeout(() => {
             $(".foto img").removeAttr('style');
+
         }, 300);
-        
+        count++;
+        $(".hit").html(count);
         
     })
 });
