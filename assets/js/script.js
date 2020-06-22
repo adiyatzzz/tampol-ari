7ff9dfba1165af1f245f46e9476a49241562a61c
adiyatzzz <orderwar0@gmail.com>
2020-06-22 14:38:18 +0800

    membuat animasi tampol

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
 
     var count = 0;
-    $(".foto img").click(function () { 
-        $(".foto img").css('animation', 'beundur .1s forwards');
-        
-
+    $(".foto").click(function () { 
+        $(".foto div").addClass("tampol-animation");
+        $(".foto").css("pointer-events", "none");
         setTimeout(() => {
-            $(".foto img").removeAttr('style');
-
-        }, 300);
-        count++;
-        $(".hit").html(count);
+            $(".foto div").removeClass("tampol-animation");
+            $(".foto").removeAttr("style");
+            count++;
+            $(".hit").html(count);
+        }, 200);
+        $("3tampol").css("opacity", "1");
+        $(".tampol").css("z-index", "3");
+        
         
     })
+
 });
