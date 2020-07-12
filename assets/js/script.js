4457b335e0d49b65a2df874141026fe34b1872de
adiyatzzz <orderwar0@gmail.com>
2020-07-12 22:40:03 +0800

    menambahkan sfx slap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
 
     var count = 0;
+    var audio = document.getElementById("sfx");
     $(".foto").click(function () { 
         $(".foto div").addClass("tampol-animation");
         $(".foto").css("pointer-events", "none");
+        audio.play();
         setTimeout(() => {
             $(".foto div").removeClass("tampol-animation");
             $(".foto").removeAttr("style");
             count++;
             $(".hit").html(count);
-        }, 300);
+            audio.pause();
+            audio.currentTime = 0;
+        }, 500);
+
         $(".tampol").css("opacity", "1");
         $(".tampol").css("z-index", "3");
         
-        
+        // setInterval(() => {
+            
+        // }, 200);
     })
 
 });
